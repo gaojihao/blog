759bc93f4ae304a533dad4c1804954d9437b765f
zhi.li <zhi.li@xjsd.com>
2022-08-12 17:04:34 +0800

    add docs

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,59 @@
+import { defineUserConfig } from 'vuepress'
+import KnznTheme from '../../src/node'
+
+export default defineUserConfig({
+  // 站点配置
+  lang: 'zh-CN',
+  title: 'lizhi1026.top',
+  description: '大前端知识分享网站',
+  // debug: true,
+  // bundler: webpackBundler(),
+  theme: KnznTheme({
+    // logo
+    logo: '/images/logo-light.svg',
+    darkLogo: '/images/logo-dark.svg',
+    // 背景图片
+    backgroundImage: '/images/bg.svg',
+    darkBackgroundImage: '/images/bg-dark.jpg',
+    // 文章简介图片
+    // postImage: 'https://v2.vuepress.vuejs.org/images/hero.png',
+    darkPostImage: '/images/post-dark.svg',
+    // 背景canvas 动画配置
+    // particlesOptions,
+
+    /**
+     * 博主信息相关
+     */
+    // 博主名称
+    blogger: '前端程序猿',
+    // 铭言
+    slogan: '种一棵树最好的时间是十年前，其次是现在！',
+    // 头像
+    avatar: '/images/avatar.jpg',
+    darkAvatar: '/images/avatar-dark.jpg',
+    // 其它媒体
+    medias: [
+      { link: 'mailto:lizhi1026@126.com', icon: 'email' },
+      { link: 'https://github.com/gaojihao', icon: 'github' },
+    ],
+
+    /**
+     * 数据显示相关
+     */
+    // 列表页显示的文章个数
+    perPage: 10,
+    // 主页显示分类的个数
+    maxCategories: 6,
+    // 主页显示的标签个数
+    maxTags: 10,
+
+    // 导航
+    // navbar: [],
+
+    /**
+     *  页脚相关配置
+     */
+    // 网址起始时间
+    siteStartDate: '2018',
+  }),
+})
